0726925bd88b8b64275e4bd62d2ec42efc1cbb0c
Simone De Cristofaro <simone.decristofaro85@gmail.com>
2024-01-11 02:41:58 +0100

    Adding code coverage and codecov ref

---
+++ scripts/create-coverage.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+set -e # Exit on error
+
+# configuration
+project=JsonMask.NET.Test
+solution="JsonMask.NET.sln"
+coverage_report_title=JsonMask.NET
+# report_assembly_filters="-ExternalServices"
+# report_class_filters="-Repository.Migrations*"
+
+usage="USAGE "$0" [-h] [OPTION] 
+-- Run tests, create the code coverage
+
+where:
+    -h, --help                    Show this help text
+    
+"
+
+function readCommandLine() {
+
+  while [ $# -ge 1 ]; do
+    key="$1"
+    case $key in
+    -h | --help)
+      echo "$usage"
+      exit 0
+      ;;
+    *)
+      # unknown option
+      ;;
+    esac
+    shift # past argument or value
+  done
+
+}
+
+function runCoverage() {
+
+  local coverage_base_dir="$project/TestResults/CodeCoverage"
+  if [ -e "$coverage_base_dir" ]; then
+    rm -rf "$coverage_base_dir"
+  fi
+
+  dotnet build $solution
+  dotnet test --settings $project/coverlet.runsettings
+
+  # get the most recent created directory
+  local dir_name=$(\ls -t $coverage_base_dir | head -n 1)
+  local coverage_output_dir="$coverage_base_dir/$dir_name"
+
+  for f in "$coverage_output_dir"/*; do
+    if [[ "$f" == *opencover.xml ]]; then
+      cp $f $coverage_base_dir/coverage-opencover.xml
+    fi
+  done
+
+  reportgenerator -reports:"$coverage_base_dir/coverage-opencover.xml" -targetdir:"CodeCoverage/output/report" -reporttypes:"TextSummary;Html" -title:$coverage_report_title -assemblyfilters:"$report_assembly_filters" -classfilters:"$report_class_filters"
+
+}
+
+readCommandLine "$@"
+runCoverage
