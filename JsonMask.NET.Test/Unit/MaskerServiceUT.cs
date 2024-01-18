c7eb143b6de9f3ed6b2678ce7da161424615fec4
Simone De Cristofaro <simone.decristofaro85@gmail.com>
2024-01-18 20:04:43 +0100

    Added MaskerService

---
+++ JsonMask.NET.Test/Unit/MaskerServiceUT.cs
@@ -0,0 +1,15 @@
+﻿
+namespace JsonMask.NET.Test.Unit
+{
+  internal class MaskerServiceUT
+  {
+
+    [Test]
+    public void MaskTest()
+    {
+      IMaskerService maskerService = new MaskerService();
+      Asserts.EqualsJToken(maskerService.Mask("{ a: 1, b: 1 }", "a"), "{ a: 1 }");
+    }
+
+  }
+}
---
+++ JsonMask.NET/MaskerService.cs
@@ -0,0 +1,18 @@
+﻿
+namespace JsonMask.NET
+{
+  public interface IMaskerService
+  {
+    string Mask(string json, string mask);
+  }
+
+  public class MaskerService : IMaskerService
+  {
+
+    public string Mask(string json, string mask)
+    {
+      return Masker.Mask(json, mask);
+    }
+
+  }
+}
